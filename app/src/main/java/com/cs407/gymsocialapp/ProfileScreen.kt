5808745e2d5e0be5f1e9ea076fa0b10896d30c02
sbail1 <stephenbail415@gmail.com>
2024-12-08 22:27:14 -0600

    profile screen edits

--- app/src/main/java/com/cs407/gymsocialapp/ProfileScreen.kt
+++ app/src/main/java/com/cs407/gymsocialapp/ProfileScreen.kt
@@ -44,6 +44,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfileScreen.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class ProfileScreen : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
